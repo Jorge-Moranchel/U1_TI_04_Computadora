de94566d80bea294c10e1feaf6b4486b9293d68f
Jorge-Moranchel <20243ds@utez.edu.mx>
2025-05-28 07:52:52 -0600

    Ya hicimos el public Computadora y hacemos el this y demas cosas pero primero el boolean es false

---
+++ Programa/src/Computadora.java
@@ -0,0 +1,13 @@
+public class Computadora {
+
+    private String marca;
+    private String modelo;
+    private Boolean encendido;
+
+    public Computadora(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.encendida = false;
+    }
+    
+}
